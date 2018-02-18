f69aff6fd9eee0db59529a2bd18dfc2b0542f170
Dániel Juhász <juhdanad@gmail.com>
2018-02-18 16:57:56 +0100

    Generate project

---
+++ desktop/src/hu/juhdanad/neonjumper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hu.juhdanad.neonjumper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hu.juhdanad.neonjumper.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
